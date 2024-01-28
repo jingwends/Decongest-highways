5381313e2bff3cb09286cb6f3d03dd6ea85f03cc
Jingwen Chen <109560483+jingwends@users.noreply.github.com>
2024-01-28 14:30:26 -0500

    Update ETL_toll_kafka_start.bash

--- ETL_toll_kafka_start.bash
+++ ETL_toll_kafka_start.bash
@@ -2,8 +2,3 @@
 cd kafka_2.12-2.8.0
 bin/kafka-topics.sh --create --topic toll --bootstrap-server localhost:9092
 
-#Download the toll_traffic_generator.py
-wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final%20Assignment/toll_traffic_generator.py
---2024-01-27 19:51:17--  https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final%20Assignment/toll_traffic_generator.py
-
-
